cb5aa8a8738960164fb1dc177276bb86e8ca7ec1
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-23 08:39:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict"; /*treat all js code as newer version*/
+
+// alert(3 + 3) we are using node js , not browser
+
+// console.log(3 + 
+//     3); code readability should be high
+
+// let Name = "noman"
+// let age = 19
+// let isLogin = false
+
+
+// Data types
+// String => ""
+// Number => 2 TO POWER 53
+// Boolean => true\false
+// BigInt
+// null => standalone value
+// undefined
+// Symbol => unique
+
+
+// Object
+// console.log(typeof undefined); output undefined
+// console.log(typeof null); output Object
+
+// Data Type conversion
+// let score = true
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = "noman"
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "noman" => true
+
+// let someNumber = true
+// let stringNumber =String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
